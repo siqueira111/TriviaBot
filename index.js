0fe1c975ab9a66bb26aa452a2a2b944e3881ebd6
root <39844782+NE137@users.noreply.github.com>
2025-04-19 18:17:24 +0200

    fix deploy

--- index.js
+++ index.js
@@ -46,6 +46,27 @@ for (const folder of commandFolders) {
 }
 
 
+// Construct and prepare an instance of the REST module
+const rest = new REST().setToken(TOKEN);
+
+// and deploy your commands!
+(async () => {
+	try {
+		console.log(`Started refreshing ${commands.length} application (/) commands.`);
+
+		// The put method is used to fully refresh all commands in the guild with the current set
+		const data = await rest.put(
+			Routes.applicationCommands(CLIENT_ID),
+			{ body: commands },
+		);
+
+		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+	} catch (error) {
+		// And of course, make sure you catch and log any errors!
+		console.error(error);
+	}
+})();
+
 cron.schedule('*/5 * * * *', async () => {
 	const channel = await client.channels.fetch(CHANNEL_ID);
 	if (!channel) return console.error('Channel not found!');
@@ -84,6 +105,7 @@ cron.schedule('*/5 * * * *', async () => {
 
 
 client.once(Events.ClientReady, readyClient => {
+	
 	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
 
